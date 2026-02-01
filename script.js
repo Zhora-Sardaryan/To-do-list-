a3c02611b383b3e16f020da2b9b85b2481a09d76
Zhora-77 <jora.sardaryan.18@gmail.com>
2026-02-01 18:22:41 +0400

    Initial todo app

---
+++ script.js
@@ -0,0 +1,31 @@
+const todoForm = document.querySelector('.todo-app__form'); 
+const todoInput = document.querySelector('.todo-app__input'); 
+const todoList = document.querySelector('.todo-app__list'); 
+
+const todoAdd = document.querySelector('.todo-app__button'); 
+
+todoForm.addEventListener('submit', function(e){ e.preventDefault();
+  const taskText = todoInput.value.trim(); 
+  if (taskText !== ""){ 
+    const listItem = document.createElement("li"); 
+    listItem.className = "todo-app__item"; 
+    const deleteButton = document.createElement("button"); 
+    deleteButton.className = "todo-app__delete"; 
+    deleteButton.innerHTML = '<ion-icon class = "icon-circle" name="ellipse-outline"></ion-icon>' + '<ion-icon class = "icon-check" name="checkmark-circle-outline"></ion-icon>'; 
+
+    const taskSpan = document.createElement('span'); 
+    taskSpan.className = 'todo-app__text'; 
+    taskSpan.textContent = taskText; 
+    listItem.appendChild(deleteButton); 
+    listItem.appendChild(taskSpan); 
+    todoList.appendChild(listItem); 
+    todoInput.value = ""; } 
+});
+
+todoList.addEventListener('click', function(e){
+ const deleteButton = e.target.closest('.todo-app__delete');
+ if (!deleteButton) return;
+  deleteButton.classList.add('pressed');
+  setTimeout(() => { deleteButton.closest('.todo-app__item').remove(); }, 150);
+ });
+
